a69ae3c53a0b97bd72d1bfa4c7683354b5d850a7
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-05-18 00:50:36 +0530

    Chnged css

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const menu = document.querySelector('.menu');
-const menuItem = document.querySelectorAll('.menuItem');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
