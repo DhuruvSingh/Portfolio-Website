94625c377e64a17d3ff6553fa8c21503c2e2fb75
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-05-18 00:45:58 +0530

    FIxed Error 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menuItem');
+const menuItem = document.querySelectorAll('.menuItem');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
