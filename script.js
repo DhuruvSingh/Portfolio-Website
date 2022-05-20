78f181b5dbbc96fecc1100654bfd9f478ca8ccb3
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-05-21 04:35:37 +0530

    Added Validation Fucntion and styled it

--- script.js
+++ script.js
@@ -326,3 +326,26 @@ openModalButtons.forEach((button) => {
     }
   });
 });
+
+// Contact form validation
+
+const submitButton = document.getElementById("send-button");
+submitButton.addEventListener("click", formSubmit);
+
+function formSubmit(event) {
+  const userName = document.getElementById("fullname").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("msg").value;
+  event.preventDefault();
+  if (email !== email.toLowerCase()) {
+    document.getElementById("error").classList.add("show-error");
+  } else {
+    const userData = JSON.stringify({
+      userName: userName,
+      email: email,
+      message: message,
+    });
+    window.localStorage.setItem("userData", userData);
+    document.getElementById("contact").reset();
+  }
+}        
